05baca921c340c63b06aceab826fae879dd4b036
Tihh Gonçalves <tihh.gon@gmail.com>
2016-08-11 10:20:45 -0300

    Ajustes Cursos

--- modules/cursos/cursos.form.php
+++ modules/cursos/cursos.form.php
@@ -4,6 +4,10 @@ $form = new nbrAdminForms('Cursos');
 $form->AddFieldString('Nome', 'Nome', 100, 3);
 $form->AddFieldLkpList('Professor', 'Professor', 'Membros', 'Nome', null, 2);
 
+$form->AddGroup('Descrição do Curso');
+$form->AddFieldString('DescricaoCurta', 'Curta', 350, 3);
+$form->AddFieldText('Descricao', 'Detalhada', 3, 200);
+
 $form->AddGroup('Imagem da Capa do Curso');
 $form->AddDescriptionText('Dimensão da capa: 1170x675px');
 $form->AddFieldImage('Capa', 'Capa');
