0c3de40d6a18c95624308704f8cda880829566b3
Tiago Gonçalves <Tiago Gonalves>
2016-08-13 16:58:03 -0300

    Ajusta de modulos para inscritos

--- modules/cursos/modulos.form.php
+++ modules/cursos/modulos.form.php
@@ -1,11 +1,6 @@
 <?
 $form = new nbrAdminForms('CursoModulos');
-
 $form->AddFieldString('Nome', 'Nome', 100, 2);
-
 $form->AddCollections('Aulas', 'aulas.grid.php', 'CursoModuloAulas', 'Modulo');
-
-
-
 $form->PrintHTML();
 ?>
