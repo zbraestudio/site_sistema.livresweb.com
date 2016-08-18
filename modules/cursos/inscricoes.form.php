ccf3f1d9c0b44482b667b88dd31a3839414b1947
Tiago Gonçalves <Tiago Gonalves>
2016-08-18 17:03:04 -0300

    Cursos

--- modules/cursos/inscricoes.form.php
+++ modules/cursos/inscricoes.form.php
@@ -4,10 +4,11 @@ $form = new nbrAdminForms('CursoInscricoes');
 $form->AddFieldLkpList('Membro', 'Membro','Membros','Nome', null, 2);
 $form->AddFieldList('Situacao', 'Situação', 'ATV=Ativo|SSP=Suspenso', 1, 'ATV');
 
-$titulo = 'Módulos';
-$descricao = 'Libere abaixo quais módulos já estão disponível para o aluno inscrito.';
-$form->AddLkpMultselect('MODULOS', $titulo, $descricao, 'CursoInscricaoModulos','Inscricao', 'CursoModulos', 'Modulo', 'Nome', 'CursoModulos.Curso =  ' . $form->record->Curso, null, 2, true, false, 'Ordem', 10, true);
-
+if($form->Editing()) {
+  $titulo = 'Módulos';
+  $descricao = 'Libere abaixo quais módulos já estão disponível para o aluno inscrito.';
+  $form->AddLkpMultselect('MODULOS', $titulo, $descricao, 'CursoInscricaoModulos', 'Inscricao', 'CursoModulos', 'Modulo', 'Nome', 'CursoModulos.Curso =  ' . $form->record->Curso, null, 2, true, false, 'Ordem', 10, true);
+}
 
 $form->PrintHTML();
 ?>
